b607dcff4dd13fc5a24a4b6a1f9ec5f419cdc267
toran billups <toranb@gmail.com>
2017-10-30 21:03:27 -0500

    REDUX: working example application

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,10 +1,20 @@
 'use strict';
 
+const glimmerRedux = require('rollup-plugin-glimmer-redux');
 const GlimmerApp = require('@glimmer/application-pipeline').GlimmerApp;
 
 module.exports = function(defaults) {
   let app = new GlimmerApp(defaults, {
-    // Add options here
+    babel: {
+      plugins: [
+        'transform-object-rest-spread',
+      ]
+    },
+    rollup: {
+      plugins: [
+        glimmerRedux()
+      ]
+    }
   });
 
   return app.toTree();
--- src/ui/components/Layout/component.ts
+++ src/ui/components/Layout/component.ts
@@ -1,5 +1,46 @@
 import Component from '@glimmer/component';
+import { ENTER } from '../../../utils/keys';
+import { connect } from 'glimmer-redux';
+import { getTodos, getFilter, getTodosCount } from '../../../reducers/todos';
 
-export default class Todomvc extends Component {
+const editTodo = (id, text) => dispatch => dispatch({type: 'EDIT_TODO', id, text});
+const addTodo = text => dispatch => dispatch({type: 'ADD_TODO', text});
+const deleteTodo = id => dispatch => dispatch({type: 'DELETE_TODO', id});
+const completeTodo = id => dispatch => dispatch({type: 'COMPLETE_TODO', id});
+const showAll = () => dispatch => dispatch({type: 'SHOW_ALL'});
+const showActive = () => dispatch => dispatch({type: 'SHOW_ACTIVE'});
+const showCompleted = () => dispatch => dispatch({type: 'SHOW_COMPLETED'});
+
+class LayoutComponent extends Component {
+
+  handleNewTodoKeyDown(event) {
+    if (event.which === ENTER) {
+      let value = event.target.value.trim();
+
+      if (value.length > 0) {
+        this.addTodo(value);
+      }
+
+      event.target.value = '';
+    }
+  }
 
 }
+
+const stateToComputed = state => ({
+  todos: getTodos(state),
+  filter: getFilter(state),
+  todosCount: getTodosCount(state)
+});
+
+const dispatchToActions = {
+  addTodo,
+  deleteTodo,
+  editTodo,
+  completeTodo,
+  showAll,
+  showActive,
+  showCompleted
+}
+
+export default connect(stateToComputed, dispatchToActions)(LayoutComponent);
